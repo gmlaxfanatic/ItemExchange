a36863d48d30c6d21d17c37072f3064e634ee01c
shadowjay1 <jrdn.ash@gmail.com>
2013-07-24 15:38:30 -0500

    Add input and output itemstacks to IETransactionEvent.

--- src/com/untamedears/ItemExchange/events/IETransactionEvent.java
+++ src/com/untamedears/ItemExchange/events/IETransactionEvent.java
@@ -4,16 +4,21 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 
 public class IETransactionEvent extends Event {
 	private static final HandlerList handlers = new HandlerList();
 
 	private Player player;
 	private Location location;
+	private ItemStack[] input;
+	private ItemStack[] output;
 
-	public IETransactionEvent(Player player, Location location) {
+	public IETransactionEvent(Player player, Location location, ItemStack[] input, ItemStack[] output) {
 		this.player = player;
 		this.location = location;
+		this.input = input;
+		this.output = output;
 	}
 
 	public Player getPlayer() {
@@ -22,6 +27,14 @@ public class IETransactionEvent extends Event {
 
 	public Location getExchangeLocation() {
 		return location;
+	}
+	
+	public ItemStack[] getInput() {
+		return input;
+	}
+	
+	public ItemStack[] getOutput() {
+		return output;
 	}
 
 	@Override
