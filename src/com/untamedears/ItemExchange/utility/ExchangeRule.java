797e402c201759fe0f122fdcf943076911ef8307
gmlaxfanatic <gmlaxfanatic@gmail.com>
2013-09-06 19:38:42 -0500

    Removed potion metadata block

--- src/com/untamedears/ItemExchange/utility/ExchangeRule.java
+++ src/com/untamedears/ItemExchange/utility/ExchangeRule.java
@@ -121,8 +121,8 @@ public class ExchangeRule {
 			else if(itemMeta instanceof EnchantmentStorageMeta) {
 				additional = new EnchantmentStorageMetadata((EnchantmentStorageMeta) itemMeta);
 			}
-
-			if(itemMeta instanceof FireworkEffectMeta || itemMeta instanceof FireworkMeta || itemMeta instanceof LeatherArmorMeta || itemMeta instanceof MapMeta || itemMeta instanceof PotionMeta || itemMeta instanceof SkullMeta) {
+			//I've removed the PotionMeta block since it is not required if only vanilla potions are used, PotionMeta support should be added in the future
+			if(itemMeta instanceof FireworkEffectMeta || itemMeta instanceof FireworkMeta || itemMeta instanceof LeatherArmorMeta || itemMeta instanceof MapMeta || itemMeta instanceof SkullMeta) {
 				throw new ExchangeRuleCreateException("This item is not yet supported by ItemExchange.");
 			}
 		}
